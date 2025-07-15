46f147d919cc5864b83989af1d36c6f75badce71
damilurg <rootdamilurg@gmail.com>
2025-07-15 17:11:54 +0600

    Added config files

---
+++ dist/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const MESSAGES = {
+    en: 'There was no error.',
+    ru: 'Не было никакой ошибки.',
+};
+const PHRASES = {
+    en: `
+💥 *FLASH*
+😎 Please remove your glasses.
+🧠 `,
+    ru: `
+💥 *ВСПЫШКА*
+😎 Снимите очки.
+🧠 `,
+};
+class Neuralyzer {
+    static configure(config) {
+        var _a, _b;
+        // Use default message for lang if no message explicitly passed
+        const lang = (_a = config.lang) !== null && _a !== void 0 ? _a : this.config.lang;
+        this.config = Object.assign(Object.assign(Object.assign({}, this.config), config), { message: (_b = config.message) !== null && _b !== void 0 ? _b : MESSAGES[lang] });
+    }
+    static flash(customMessage) {
+        const { message, duration, cssClass, log, lang } = this.config;
+        const flashEl = document.createElement('div');
+        flashEl.classList.add(cssClass);
+        document.body.appendChild(flashEl);
+        setTimeout(() => {
+            flashEl.remove();
+            if (log) {
+                console.debug(`${PHRASES[lang]}${customMessage || message}`);
+            }
+        }, duration);
+    }
+}
+Neuralyzer.config = {
+    message: MESSAGES.en,
+    duration: 1000,
+    cssClass: 'neuralyzer-flash',
+    log: true,
+    lang: 'en',
+};
+
+module.exports = Neuralyzer;
